621d2c0142fc46e5434946457eeb714bfe2073ba
Taump <taump@yandex.ru>
2020-05-13 15:48:43 +0700

    init store

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const App = () => <div className="App">init</div>;
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import "antd/dist/antd.css";
+import configureStore from "./store";
+
+import { AppRouter } from "./AppRouter";
+
+const { persistor, store } = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AppRouter />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
